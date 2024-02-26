1bf0f600c0a9787e2ce1cd560b1a5bd52027b2e7
NeelavaHBR <hbr.neelava@gmail.com>
2024-02-26 18:30:36 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+
+const mongodb = require ("./db")
+const express = require('express')
+const app = express()
+const cors = require("cors")
+
+const port = 6000
+mongodb()
+
+// app.use((req, res, next)=>{
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// })
+
+app.use(
+  cors({
+    origin:"http://localhost:3000",
+  })
+  );
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+app.use(express.json())
+app.use("/api", require("./Routes/CreateUser"));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
